830bf3c17111d7159a9a4b82892a3696bdc372b0
Pedro <pedrohmendescandido@gmail.com>
2025-09-26 08:22:43 -0300

    Criação da Landing Page e rotas para ela

--- front-end/digital-twin/src/App.jsx
+++ front-end/digital-twin/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import LoginPage from './pages/LoginPage/LoginPage.jsx'
 import RegisterPage from './pages/RegisterPage/RegisterPage.jsx'
+import LandingPage from './pages/LandingPage/LandingPage.tsx'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
+        <Route path="/landing-page" element={<LandingPage />} />
       </Routes>
     </Router>
   )
--- front-end/digital-twin/src/pages/RegisterPage/RegisterPage.jsx
+++ front-end/digital-twin/src/pages/RegisterPage/RegisterPage.jsx
@@ -1,10 +1,17 @@
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 export default function RegisterPage() {
+  const navigate = useNavigate()
+
+  const handleRegister = (e) => {
+    e.preventDefault()
+    navigate('/landing-page')
+  }
+
   return <>
     <h1>Bem Vindo à página de Registro</h1>
     <p>Preencha os campos para criar uma nova conta</p>
-    <form action="">
+    <form onSubmit={handleRegister}>
         <div>
             <label htmlFor="username">Usuário:</label>
             <input type="text" id="username" name="username" />
@@ -17,6 +24,7 @@ export default function RegisterPage() {
             <label htmlFor="password">Senha:</label>
             <input type="password" id="password" name="password" />
         </div>
+        <button type="submit">Registrar</button>
     </form>
     <p>Já possui uma conta? <Link to="/login">Faça login</Link></p>
   </>;
