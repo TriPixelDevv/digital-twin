4e6c5a7d4596a6e24cd2cc9f7b8bac5f91a3ccba
Kaiki <kaikiandrade64@gmail.com>
2025-09-29 20:20:50 -0300

    adicionado o front novamente

---
+++ frontend/digital-twin/src/pages/LandingPage/LandingPage.tsx
@@ -0,0 +1,9 @@
+export default function LandingPage() {
+    return <>
+        <h1>Bem Vindo Ao Digital Twin do seu Carro!</h1>
+        <p>Nome do Carro: Toyota</p>
+        <p>Modelo: Corolla</p>
+        <p>Ano: 2020</p>
+        <p>Para visualizar os dados do seu carro acesse o nosso <a href="/dashboard">painel</a>.</p>
+    </>
+}
